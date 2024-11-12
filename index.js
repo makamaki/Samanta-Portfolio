1f2f8475c673931f916ae6e6deb7d9a2a61541ec
makamaki <119682810+makamaki@users.noreply.github.com>
2024-11-12 15:03:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+function toggleMenu() {
+    const body = document.querySelector('body');  // Or any parent element
+    body.classList.toggle('nav-open');  // Toggle the nav-open class
+}
